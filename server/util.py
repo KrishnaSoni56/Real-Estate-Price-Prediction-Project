78e0c7906fb019b7d55692cde870584e8e62e786
KrishnaSoni56 <soni2005krishna26@gmail.com>
2024-06-16 12:21:15 +0530

    Created a backend server for predicting prices

--- server/util.py
+++ server/util.py
@@ -6,12 +6,12 @@ __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price(location,sqft,BHK,bath):
+def get_estimated_price(location,sqft,bhk,bath):
     try:
         loc_index = __data_columns.index(location.lower())
     except:
         loc_index = -1
- 
+
     x = np.zeros(len(__data_columns))
     x[0] = sqft
     x[1] = bath
@@ -27,13 +27,13 @@ def load_saved_artifacts():
     global  __data_columns
     global __locations
 
-    with open("./columns.json", "r") as f:
+    with open("/home/krishna/python_jupyter/Real-Estate-Price-Prediction-Project/server/artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
 
     global __model
     if __model is None:
-        with open('./banglore_home_prices_model.pickle', 'rb') as f:
+        with open('/home/krishna/python_jupyter/Real-Estate-Price-Prediction-Project/server/artifacts/banglore_home_prices_model.pickle', 'rb') as f:
             __model = pickle.load(f)
     print("loading saved artifacts...done")
 
